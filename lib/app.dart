791efaefa2029ff27735be38dda442bef870f71c
M. Rifki Paranrengi <rifkip1337@gmail.com>
2025-09-18 22:53:17 +0800

    (#5) MDC-104 FINISH !
    feat(app): migrate branding and enhance category navigation
    
    Updates app branding to "acatel" and refactors core navigation to support dynamic category selection via a stateful main widget and backdrop layout.
    
    - AndroidManifest.xml:
      - [~] Change app label to "acatel"
    - lib/app.dart:
      - [~] Convert main widget to stateful for category handling
      - [~] Integrate backdrop navigation with menu and category selection
      - [~] Update titles to "ACATEL" and "MENU"
    - lib/home.dart:
      - [~] Simplify home page to use AsymmetricView and accept category
      - [~] Remove legacy grid/card UI logic
    - lib/supplemental/product_columns.dart:
      - [~] Use ListView instead of Column for product card layouts
      - [~] Add scroll physics and handle reversed list for single product columns
    - pubspec.yaml:
      - [~] Change app name to "acatel"
      - [~] Update dependencies: intl and flutter_lints
      - [+] Add slanted_menu.png asset
    
    Improves code organization and prepares for category-based browsing.

--- lib/app.dart
+++ lib/app.dart
@@ -11,18 +11,31 @@
 // WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
 // See the License for the specific language governing permissions and
 // limitations under the License.
-
+import 'backdrop.dart';
+import 'category_menu_page.dart';
 import 'colors.dart';
 import 'package:flutter/material.dart';
-
 import 'home.dart';
 import 'login.dart';
-
+import 'model/product.dart';
 import 'supplemental/cut_corners_border.dart';
 
 // TODO: Convert ShrineApp to stateful widget (104)
-class ShrineApp extends StatelessWidget {
+class ShrineApp extends StatefulWidget {
   const ShrineApp({Key? key}) : super(key: key);
+
+  @override
+  State<ShrineApp> createState() => _ShrineAppState();
+}
+
+class _ShrineAppState extends State<ShrineApp> {
+  Category _currentCategory = Category.all;
+
+  void _onCategoryTap(Category category) {
+    setState(() {
+      _currentCategory = category;
+    });
+  }
 
   @override
   Widget build(BuildContext context) {
@@ -32,10 +45,19 @@ class ShrineApp extends StatelessWidget {
       routes: {
         '/login': (BuildContext context) => const LoginPage(),
         // TODO: Change to a Backdrop with a HomePage frontLayer (104)
-        '/': (BuildContext context) => const HomePage(),
-        // TODO: Make currentCategory field take _currentCategory (104)
-        // TODO: Pass _currentCategory for frontLayer (104)
-        // TODO: Change backLayer field value to CategoryMenuPage (104)
+        '/': (BuildContext context) => Backdrop(
+              // TODO: Make currentCategory field take _currentCategory (104)
+              currentCategory: _currentCategory,
+              // TODO: Pass _currentCategory for frontLayer (104)
+              frontLayer: HomePage(category: _currentCategory),
+              // TODO: Change backLayer field value to CategoryMenuPage (104)
+              backLayer: CategoryMenuPage(
+                currentCategory: _currentCategory,
+                onCategoryTap: _onCategoryTap,
+              ),
+              frontTitle: const Text('ACATEL'),
+              backTitle: const Text('MENU'),
+            ),
       },
       // TODO: Customize the theme (103)
       theme: _kShrineTheme,
@@ -62,7 +84,6 @@ ThemeData _buildShrineTheme() {
       foregroundColor: kShrineBrown900,
       backgroundColor: kShrinePink100,
     ),
-
     inputDecorationTheme: const InputDecorationTheme(
       border: CutCornersBorder(),
       focusedBorder: CutCornersBorder(
